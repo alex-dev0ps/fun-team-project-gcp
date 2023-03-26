601543392eb110585494c3b29d00dff009baf8fe
Ekaterina Veselova <kathleen.veselova@gmail.com>
2023-03-26 04:21:35 +0000

    files added

---
+++ KatyaVPCmodule/main.tf
@@ -0,0 +1,11 @@
+ # module "vpc" {
+     # source = terraform registry
+ # }
+
+resource "google_compute_network" "vpc" {         
+     name = "fun-team-project-gpc"
+     auto_create_subnetworks = "true"
+     routing_mode = "GLOBAL"
+ }
+
+
