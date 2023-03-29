d27487cde90c6c810cb47572086d007af939c359
Ekaterina Veselova <kathleen.veselova@gmail.com>
2023-03-29 04:32:32 +0000

    Makefile for vpc added

---
+++ mymodule/module/makefile/provider.tf
@@ -0,0 +1,9 @@
+# locals {
+# project = "fun-team-project-gcp"
+# region = "us-central1"
+# }
+
+provider "google" {
+  region = var.region
+  zone   = var.zone
+}
---
+++ mymodule/module/makefile/variable.tf
@@ -0,0 +1,15 @@
+/* variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-a"
+} */
+
+variable "region" {
+  description = "Please provide a region"
+}
+
+variable "zone" {
+  description = "Please provide a zone"
+}
